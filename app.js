f008eebc7e8de63e0947c2d270e451efe5cedb58
kdunphe <57336712+kdunphe@users.noreply.github.com>
2020-08-30 17:52:10 -0400

    Updating app.js.

--- app.js
+++ app.js
@@ -4,11 +4,108 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const render = require("./lib/htmlRenderer");
+let employees = [];
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+
+// array of questions for employees
+const employeeQuestions = [{
+    type: "input",
+    name: "Name",
+    message: "Enter the employees name.",
+  
+  },
+  {
+    type: "input",
+    name: "ID",
+    message: "Enter the employees ID.",
+  },
+  {
+    type: "input",
+    name: "Email",
+    message: "Enter the employees email address.",
+  }];
+
+  const managerQuestions = [
+  {
+    type: "input",
+    name: "OfficeNumber",
+    message: "Enter the managers office number.",
+  }];
+
+  const engineerQuestions = [
+    {
+      type: "input",
+      name: "GitHubUsername",
+      message: "Enter the engineers GitHub username.",
+    }];
+
+  const internQuestions = [
+    {
+      type: "input",
+      name: "School",
+      message: "Enter the interns school.",
+    }];
+
+  const roleQuestions = [
+    {
+      type: "list",
+      name: "Role",
+      message: "Select the employees role.",
+      choices: [
+          "Manager",
+          "Engineer",
+          "Intern",
+      ]
+    }];
+
+
+// Source: https://www.npmjs.com/package/inquirer
+// Source: https://jestjs.io/docs/en/getting-started.html
+
+
+
+  
+  
+  // function to initialize program
+  // Source: https://www.npmjs.com/package/inquirer
+//   function init() {
+//     return inquirer
+//       .prompt(questions)
+//       .then(answers => {
+//         console.log(answers);
+//         // Format responses in markdown
+//         let markdown = formatText(answers);
+//         // Call a function and return a string with markdown text
+//         fs.writeFile('ReadMe-Demo.md', markdown, function (err) {
+//           if (err) throw err;
+//           console.log("Done! Successfully created ReadMe-Demo.md");
+//         });
+//       })
+  
+//       .catch(err => {
+//         console.log(err)
+//       });
+//   }
+  
+  
+//   // function call to initialize program
+//   init();
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Write code to use inquirer to gather information about the development team members,
